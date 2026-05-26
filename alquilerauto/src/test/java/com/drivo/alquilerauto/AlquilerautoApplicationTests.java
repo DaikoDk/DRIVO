f0cc6639e3e2d823f90fc3f72f0829b1e27945f9
DaikoDk <djbarcave@gmail.com>
2026-05-25 23:18:56 -0500

    Initial commit: Spring Boot backend + Angular 21 frontend - AlquilerAuto (DRIVO)

---
+++ alquilerauto/src/test/java/com/drivo/alquilerauto/AlquilerautoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drivo.alquilerauto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlquilerautoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
